c6253b3a6b1f0452914a2dad5c768aeeb435d835
shmo <evgeny.a.naumov@gmail.com>
2017-11-22 19:25:59 -0500

    versioned release

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+# semver with automatic minor bumps keyed to unix time
+__version__ = '1.0.1511396759'
+
+
+setup(
+    name='py9backup',
+    version=__version__,
+    packages=[],
+    scripts=['py9backup/backup'],
+    install_requires=['click'],
+)
